3cb7f156cd47ee637f0c194313346c8b8282cca5
Rafael <rafaelpmeirelles@gmail.com>
2023-09-14 15:54:18 -0300

    Solução construtiva

--- main.jl
+++ main.jl
@@ -1,1 +1,13 @@
-println("Hello, World!")
+using TSPLIB
+
+function constructiveSolution(tsp, startingNode)
+    remainingNodes = Set(0:tsp.dimension)
+    cost = 0
+    delete!(remainingNodes, startingNode)
+
+
+end
+
+push!(visitedNodes, initialNode)
+tsp = readTSP("TSP-MetaHeuristics/data/xqf131.tsp")
+

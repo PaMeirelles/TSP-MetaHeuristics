e6a8edd1c4d30d7205099366c8ba4016c4444faa
Rafael <rafaelpmeirelles@gmail.com>
2023-09-17 14:10:55 -0300

    Removing last snake_case instances

--- LocalSearch.jl
+++ LocalSearch.jl
@@ -2,36 +2,36 @@ using TSPLIB
 
 module LocalSearch
     using ..TSPLIB
-    export first_improvement
+    export firstImprovement
 
-    function node_cost(solution::Vector{Int}, node::Int, tsp::TSP)::Float64
+    function nodeCost(solution::Vector{Int}, node::Int, tsp::TSP)::Float64
         prev = max(1, node-1)
         next = min(length(solution), node+1)
 
         return tsp.weights[solution[prev], solution[node]] + tsp.weights[solution[node], solution[next]]
     end
 
-    function perform_swap(solution::Vector{Int}, node_1::Int, node_2::Int, tsp::TSP)::Float64
-        prev_cost = node_cost(solution, node_1, tsp) + node_cost(solution, node_2, tsp)
+    function performSwap(solution::Vector{Int}, node1::Int, node2::Int, tsp::TSP)::Float64
+        prevCost = nodeCost(solution, node1, tsp) + nodeCost(solution, node2, tsp)
 
-        solution[node_1], solution[node_2] = solution[node_2], solution[node_1]
+        solution[node1], solution[node2] = solution[node2], solution[node1]
 
-        new_cost = node_cost(solution, node_1, tsp) + node_cost(solution, node_2, tsp)
+        newCost = nodeCost(solution, node1, tsp) + nodeCost(solution, node2, tsp)
 
-        return new_cost - prev_cost
+        return newCost - prevCost
     end
 
-    function first_improvement(solution::Vector{Int}, tsp::TSP, cost::Float64, verbose::Bool=false)::Tuple{Vector{Int}, Float64}
+    function firstImprovement(solution::Vector{Int}, tsp::TSP, cost::Float64, verbose::Bool=false)::Tuple{Vector{Int}, Float64}
         for i in 1:length(solution)
             for j in i:length(solution)
-                delta = perform_swap(solution, i, j, tsp)
+                delta = performSwap(solution, i, j, tsp)
                 if (delta < 0)
                     if verbose
                         println("Score improved from $cost to $(cost+delta) by swapping $i and $j")
                     end
-                    return first_improvement(solution, tsp, cost+delta, verbose)
+                    return firstImprovement(solution, tsp, cost+delta, verbose)
                 else
-                    perform_swap(solution, j, i, tsp)
+                    performSwap(solution, j, i, tsp)
                 end
             end
         end

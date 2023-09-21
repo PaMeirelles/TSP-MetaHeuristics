483b4b8566de160c9b5abd923dacc0fe69721798
Lucas Guilhon <lximenes99@gmail.com>
2023-09-21 09:08:52 -0300

    [#5] Create relocate struct

---
+++ src/Relocate.jl
@@ -0,0 +1,18 @@
+struct Relocate
+    data::TSP
+    solution::Solution
+ end
+
+ 
+ function eval(relocate::Relocate, pos1::Int64, pos2::Int64)::Int64
+ end
+ 
+ function move!(relocate::Relocate, pos1::Int64, pos2::Int64)
+ end
+ 
+ function firstImprovement!(relocate::Relocate)::Bool
+ end
+
+ function bestImprovement!(relocate::Relocate)::Bool
+end
+

5c10e73eccdf050a2f1b237f9c081586b972c3eb
Rafael <rafaelpmeirelles@gmail.com>
2023-09-21 21:58:17 -0300

    [#5] Relocate

--- main.jl
+++ main.jl
@@ -6,6 +6,7 @@ include("src/Swap.jl")
 include("src/Utils.jl")
 include("src/Benchmark.jl")
 include("src/TwoOpt.jl")
+include("src/Relocate.jl")
 
 function testCase(instance::TSP)
     sol, cost = basicGreedy(instance)
@@ -22,15 +23,19 @@ function testCase(instance::TSP)
     end
 end
 
-instance = readTSPLIB(:a280)
+instance = readTSPLIB(:eil101)
 # testCase(instance)
 
 n1 = Swap(instance, basicGreedy(instance))    
 n2 = Swap(instance, basicGreedy(instance))   
 n3 = TwoOpt(instance, basicGreedy(instance))    
 n4 = TwoOpt(instance, basicGreedy(instance))  
+n5 = Relocate(instance, basicGreedy(instance))    
+n6 = Relocate(instance, basicGreedy(instance))  
 
 benchmark(firstImprovement!, n1)
 benchmark(bestImprovement!, n2)
 benchmark(firstImprovement!, n3)
 benchmark(bestImprovement!, n4)
+benchmark(firstImprovement!, n5)
+benchmark(bestImprovement!, n6)
